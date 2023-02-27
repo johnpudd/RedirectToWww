1d55a05b94864e67452e3545f8463edd74f72c82
John Puddifoot <john@eastpoint.co.uk>
2023-02-27 13:00:15 +0000

    Tweaks

--- RedirectToWww/Program.cs
+++ RedirectToWww/Program.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Rewrite;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
+
 app.UseRewriter(
     new RewriteOptions()
         .AddRedirectToHttpsPermanent()
         .AddRedirectToWwwPermanent()
         );
+app.MapGet("/", () =>
+{   
+    return "Hello redirects";
+});
+
 app.Run();
